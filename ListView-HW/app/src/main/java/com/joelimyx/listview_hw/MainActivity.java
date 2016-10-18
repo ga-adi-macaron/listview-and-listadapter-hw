436623024e9c450858d924c314065e6883c1a6aa
Joe Lim <joelimyx@gmail.com>
2016-10-17 21:03:24 -0400

    Finish basic homework

--- ListView-HW/app/src/main/java/com/joelimyx/listview_hw/MainActivity.java
+++ ListView-HW/app/src/main/java/com/joelimyx/listview_hw/MainActivity.java
@@ -2,12 +2,87 @@ package com.joelimyx.listview_hw;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    //Initialization
+    private ListView mListView;
+    private Button mAddButton, mRemoveButton;
+    private EditText mEditText;
+    List<Player> mPlayerList;
+    BaseAdapter myAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mListView = (ListView) findViewById(R.id.list_view);
+        mAddButton = (Button) findViewById(R.id.addButton);
+        mRemoveButton = (Button) findViewById(R.id.removeButton);
+        mEditText = (EditText) findViewById(R.id.editText);
+        mPlayerList = new ArrayList<>();
+
+        myAdapter = new BaseAdapter() {
+            @Override
+            public int getCount() {
+                return mPlayerList.size();
+            }
+
+            @Override
+            public Object getItem(int position) {
+                return mPlayerList.get(position);
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return position;
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                if(convertView==null){
+                    LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
+                    convertView = inflater.inflate(R.layout.player_adapter,null);
+                }
+                TextView textView = (TextView) convertView.findViewById(R.id.text1);
+                textView.setText(mPlayerList.get(position).getName());
+                return convertView;
+            }
+        };
+
+        mListView.setAdapter(myAdapter);
+        mAddButton.setOnClickListener(this);
+        mRemoveButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.addButton:
+                String temp = mEditText.getText().toString();
+                mPlayerList.add(new Player(temp));
+                myAdapter.notifyDataSetChanged();
+                break;
+            case R.id.removeButton:
+                if (!mPlayerList.isEmpty()) {
+                    mPlayerList.remove(mPlayerList.size() - 1);
+                    myAdapter.notifyDataSetChanged();
+                }
+                break;
+        }
     }
 }
---
+++ ListView-HW/app/src/main/java/com/joelimyx/listview_hw/Player.java
@@ -0,0 +1,22 @@
+package com.joelimyx.listview_hw;
+
+/**
+ * Created by Joe on 10/17/16.
+ */
+
+public class Player {
+    private String mName;
+    private String mRole;
+
+    public Player(String name) {
+        mName = name;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+}
